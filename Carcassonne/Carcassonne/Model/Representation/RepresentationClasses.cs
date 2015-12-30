06bf0cdb1e8806d9c14fdb279aa303faef53735f
Csernák Tamás <altvik26@gmail.com>
2015-12-30 14:55:29 +0100

    Separated helper classes into their own file.

---
+++ Carcassonne/Carcassonne/Model/Representation/RepresentationClasses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcassonne.Model.Representation
+{
+    #region Tile
+    public struct TileSideDescriptor
+    {
+        public TileSideDescriptor(TileSideType up, TileSideType down, TileSideType left, TileSideType right)
+        {
+            Up = up;
+            Down = down;
+            Left = left;
+            Right = right;
+        }
+        public TileSideType Up, Down, Left, Right;
+    }
+
+    public enum TileSideType
+    {
+        Field,
+        Road,
+        Castle
+    }
+
+    public enum TileFieldType
+    {
+        Road,
+        Field,
+        Castle,
+        Monastery
+    }
+    #endregion Tile
+}
