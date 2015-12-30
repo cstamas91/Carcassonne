ba1b4d36bc66b9052842f7253d0b8e29ca299cc9
Csernák Tamás <altvik26@gmail.com>
2015-12-30 14:57:25 +0100

    Added class for gametable representation.

---
+++ Carcassonne/Carcassonne/Model/Representation/GameTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcassonne.Model.Representation
+{
+    public class GameTable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
