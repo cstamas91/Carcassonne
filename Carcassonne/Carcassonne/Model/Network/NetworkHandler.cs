9a989eb3529bf3418c9a4a8e8c4bae2fb66e9000
Csernák Tamás <altvik26@gmail.com>
2016-01-31 17:26:43 +0100

    Hálózatkezelő osztály és kiegészítések hozzáadása

---
+++ Carcassonne/Carcassonne/Model/Network/NetworkHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcassonne.Model.Network
+{
+    /// <summary>
+    /// Hálózati kommunikációért felelős osztály.
+    /// </summary>
+    public class NetworkHandler
+    {
+
+        #region Declarations
+        private List<TcpClient> clients;
+        private List<TcpListener> listeners;
+        /// <summary>
+        /// Hálózati kommunikáció kezelőjének a konstruktora.
+        /// </summary>
+        public NetworkHandler() 
+        {
+            
+        }
+        #endregion Declarations
+
+        #region Events
+
+        public event NewGameRequestReceivedHandler NewGameRequestReceived;
+        public delegate void NewGameRequestReceivedHandler(object sender, NewGameRequestReceivedEventArgs e);
+        public 
+
+        #endregion Events
+    }
+}
---
+++ Carcassonne/Carcassonne/Model/Network/NetworkHandlerEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcassonne.Model.Network
+{
+    public class NewGameRequestReceivedEventArgs : EventArgs 
+    {
+        public string GameHandle { get; set; }
+        public string GameGuid { get; set; }
+        public string PlayerName { get; set; }
+        public int NumberOfPlayers { get; set; }
+
+        public NewGameRequestReceivedEventArgs(string handle, string guid, string playerName, int numberOfPlayers)
+        {
+            this.GameGuid = guid;
+            this.GameHandle = handle;
+            this.PlayerName = playerName;
+            this.NumberOfPlayers = numberOfPlayers;
+        }
+    }
+}
