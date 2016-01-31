af82cad468234f02a439d090e7d5f7d9ee07cc5d
Csernák Tamás <altvik26@gmail.com>
2016-01-31 17:27:20 +0100

    Játék és játékkiszolgáló osztályok hozzáadása

---
+++ Carcassonne/Carcassonne/Model/Representation/GameServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carcassonne.Model.Network;
+
+namespace Carcassonne.Model.Representation
+{
+    public class GameServer
+    {
+        #region Declarations
+        private Dictionary<string, Game> games;
+        private NetworkHandler networkHandler;
+
+        public GameServer()
+        {
+            games = new Dictionary<string, Game>();
+            networkHandler = new NetworkHandler();
+
+            networkHandler.NewGameRequestReceived += NetworkHandler_NewGameRequestHandler;
+        }
+        #endregion Declarations
+
+        #region handlers
+        public void NetworkHandler_NewGameRequestHandler(object sender, NewGameRequestReceivedEventArgs e) 
+        {
+            var newGame = new Game(e.NumberOfPlayers, e.GameGuid);
+            newGame.AddPlayer(new Player(0, e.PlayerName));
+            
+            games.Add(newGame.Guid, newGame);
+        }
+        #endregion handlers
+
+        #region Private methods
+        #endregion Private methods
+    }
+}
