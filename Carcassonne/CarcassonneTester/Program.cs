ccd33e1b67a990fe7e32a3d6e4d0aa30cfa27244
Csernák Tamás <altvik26@gmail.com>
2016-04-05 12:34:17 +0200

    Teszt és UnitTest projektek hozzáadása.

---
+++ Carcassonne/CarcassonneTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using Carcassonne.Model.Representation;
+using Carcassonne.Model.Tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarcassonneTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> ls = new List<Player>() { new Player(0, "Tomi"), new Player(1, "pwnzor") };
+            InitContent ic = new InitContent(ls);
+            using (var stream = new MemoryStream())
+            {
+                ic.WriteContent(stream);
+
+                var readIC = PayloadContentFactory<InitContent>.Create(stream.ToArray());
+                foreach (var playa in readIC.Players)
+                    Console.WriteLine(playa);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ Carcassonne/CarcassonneUnitTest/PayloadTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Carcassonne.Model.Representation;
+using Carcassonne.Model.Tools;
+using System.Text;
+using System.IO;
+
+namespace CarcassonneUnitTest
+{
+    [TestClass]
+    public class PayloadTest
+    {
+        [TestMethod]
+        public void PlayerWrite()
+        {
+            var one = BitConverter.GetBytes((short)1);
+            var playa1 = Encoding.Default.GetBytes("Playa1\n");
+            var expected = new byte[one.Length + playa1.Length];
+            Buffer.BlockCopy(one, 0, expected, 0, one.Length);
+            Buffer.BlockCopy(playa1, 0, expected, one.Length, playa1.Length);
+            var player = new Player(1, "Playa1");
+            var actual = new byte[short.MaxValue];
+
+            using (var ms = new MemoryStream())
+            {
+                player.WriteContent(ms);
+                actual = ms.ToArray();   
+            }
+            var actualPlayer = PayloadContentFactory<Player>.Create(actual);
+            
+            CollectionAssert.AreEqual(expected, actual);
+            Assert.AreNotSame(player, actualPlayer);
+            Assert.AreEqual<short>(player.Number, actualPlayer.Number);
+            Assert.AreEqual<string>(player.Name, actualPlayer.Name);
+        }
+
+        [TestMethod]
+        public void WriteShortTest()
+        {
+            var val = BitConverter.GetBytes((short)15);
+            var min = BitConverter.GetBytes((short)short.MinValue);
+            var max = BitConverter.GetBytes((short)short.MaxValue);
+            var actualVal = new byte[sizeof(short)];
+            var actualMin = new byte[sizeof(short)];
+            var actualMax = new byte[sizeof(short)];
+
+            using (var ms = new MemoryStream())
+            {
+                ms.WriteShort(15);
+                actualVal = ms.ToArray();
+
+                ms.Position = 0;
+                ms.WriteShort(short.MinValue);
+                actualMin = ms.ToArray();
+
+                ms.Position = 0;
+                ms.WriteShort(short.MaxValue);
+                actualMax = ms.ToArray();
+            }
+
+
+            CollectionAssert.AreEqual(max, actualMax);
+            CollectionAssert.AreEqual(min, actualMin);
+            CollectionAssert.AreEqual(val, actualVal);
+        }
+    }
+}
