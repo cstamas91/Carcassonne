e45e1c321a0891fde43da6547407e7d45a86ae0b
Csernák Tamás <altvik26@gmail.com>
2016-04-18 16:01:48 +0200

    Projekt restrukturálás

---
+++ Carcassonne/CarcassonneServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarcassonneSharedModules.Logger;
+
+namespace CarcassonneServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LogToConsole = true;
+            Logger.WriteLog("Server started.");
+            Logger.WriteLog("Server stopping.");
+            Logger.EndLog();
+        }
+    }
+}
