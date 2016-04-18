24600f0bae9d399bee9ee8accced0d680936892f
Csernák Tamás <altvik26@gmail.com>
2016-04-18 16:02:56 +0200

    Projekt restrukturálás

---
+++ Carcassonne/CarcassonneSharedModules/Logger/Logger.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CarcassonneSharedModules.Logger
+{
+    public static class Logger
+    {
+        private static FileLogger fileLogger;
+        private static ConsoleLogger consoleLogger;
+        public static bool LogToConsole
+        {
+            get;
+            set;
+        }
+
+        static Logger()
+        {
+            fileLogger = new FileLogger();
+            consoleLogger = new ConsoleLogger();
+        }
+
+        public static void WriteLog(string message)
+        {
+            fileLogger.WriteLog(message);
+            if (LogToConsole)
+                consoleLogger.WriteLog(message);
+        }
+
+        public static Task EndLog()
+        {
+            return Task.WhenAll(fileLogger.EndLog(), consoleLogger.EndLog());
+        }
+    }
+
+    public abstract class AbstractLogger
+    {
+        protected virtual TextWriter LogWriter { get { return null; } }
+
+        public virtual void WriteLog(string message)
+        {
+            LogWriter.WriteLine(string.Format("{0}|{1}", DateTime.Now.ToString("hh:mm:ss.fff"), message));
+        }
+    }
+
+    public class FileLogger : AbstractLogger
+    {
+        private DateTime logStartTime;
+
+        private Stream logStream = null;
+        private TextWriter logWriter = null;
+        protected override TextWriter LogWriter
+        {
+            get
+            {
+                if (logWriter == null)
+                {
+                    if (logStream == null)
+                        logStream = File.Create(BuildLogfileName());
+
+                    logWriter = new StreamWriter(logStream);
+                }
+
+                return logWriter;
+            }
+        }
+
+        protected static string LogPath
+        {
+            get
+            {
+                var path = string.Format(@"{0}\Carcassonne\Logs\", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                return path;
+            }
+            set { }
+        }
+
+        public FileLogger()
+        {
+            logStartTime = DateTime.Now;
+        }
+
+        public async Task EndLog()
+        {
+            try
+            {
+                var str = BuildLogfileName();
+                using (var logFile = File.Create(str))
+                {
+                    await logStream.CopyToAsync(logFile);
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+                logWriter.Dispose();
+            }
+        }
+
+        private string BuildLogfileName()
+        {
+            return string.Format(@"{0}{1}__{2}.log", LogPath, DateTime.Today.ToString("yyyy-MM-dd"), logStartTime.ToString("hh-mm-ss-fff"));
+        }
+    }
+
+    public class ConsoleLogger : AbstractLogger
+    {
+        protected override TextWriter LogWriter { get { return Console.Out; } }
+
+        public ConsoleLogger() { }
+        
+        public Task EndLog()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Carcassonne/CarcassonneSharedModules/Tools/InitContent.cs
@@ -0,0 +1,71 @@
+﻿using CarcassonneSharedModules.Representation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CarcassonneSharedModules.Tools
+{
+    /// <summary>
+    /// A játék inicializáláshoz szükséges információkat tartalmazó osztály. A kliensek a játék kezdete előtt megkapják.
+    /// </summary>
+    public class InitContent : IPayloadContent
+    {
+        public IEnumerable<Player> Players { get; private set; }
+
+        public InitContent() { }
+        public InitContent(IEnumerable<Player> players)
+        {
+            Players = players;
+        }
+
+        #region IPayloadContent
+
+        public void ReadContent(byte[] payloadContent)
+        {
+            var ls = new List<Player>();
+            using (var ms = new MemoryStream(payloadContent))
+            {
+                var count = new byte[sizeof(short)];
+                ms.Read(count, 0, sizeof(short));
+
+                for (int i = 0; i < BitConverter.ToInt16(count, 0); i++)
+                {
+                    ls.Add(PayloadContentFactory<Player>.Create(ms.ReadToChar('\n', 2)));
+
+                    #region for reference
+                    //using (var playerStream = new MemoryStream()) //stream for playerdata
+                    //{
+                    //    var buff = new byte[2];
+                    //    ms.Read(buff, 0, 1);
+                    //    var c = BitConverter.ToChar(buff, 0);
+                    //    while (c != '\n')
+                    //    {
+                    //        playerStream.Write(buff, 0, 1);
+                    //        ms.Read(buff, 0, 1);
+                    //        c = BitConverter.ToChar(buff, 0);
+                    //    }
+                        
+                    //    var arr = playerStream.ToArray();
+                    //    ls.Add(PayloadContentFactory<Player>.Create(arr));
+                    //}
+                    #endregion for reference
+                }
+            }
+            Players = ls;
+        }
+
+        public void WriteContent(Stream contentStream)
+        {
+            var playerList = new List<Player>(Players);
+
+            contentStream.WriteShort((short)playerList.Count);
+            foreach (var player in playerList)
+                player.WriteContent(contentStream);
+        }
+
+        #endregion IPayloadContent
+    }
+}
