7f01749ab3700c69781da728f2ebdaee5d961838
Csernák Tamás <altvik26@gmail.com>
2016-04-26 03:24:35 +0200

    Projekt restrukturálás

--- Carcassonne/CarcassonneServer/Model/GameLogic/Model.cs
+++ Carcassonne/CarcassonneServer/Model/GameLogic/Model.cs
@@ -1,43 +1,34 @@
 ﻿using CarcassonneSharedModules.Network;
-using CarcassonneSharedModules.Representation;
 using CarcassonneSharedModules.Tools;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CarcassonneServer.Model.Representation;
 
 namespace CarcassonneServer.Model.GameLogic
 {
-    
-    public class GameModel : IGameModel
+
+    public class GameModel
     {
         private TurnHandler turnHandler;
         private ScoreHandler scoreHandler;
-        private GameTable gameTable;
         private NetworkHandler networkHandler;
-        private PayloadContentHandler payloadContentHandler;
         private IEnumerable<Player> players;
 
-        public GameTable GameTable { get { return gameTable; } }
         public ScoreHandler ScoreHandler { get { return scoreHandler; } }
         public IEnumerable<Player> Players { get { return players; } }
         /// <summary>
         /// Property a játék aktuális állapotának lekérésére.
         /// </summary>
-        public GameStateDescriptor State { get { return GameStateDescriptor.GameStateDescriptorFactory(this); } }
+        //public GameStateDescriptor State { get { return GameStateDescriptor.GameStateDescriptorFactory(this); } }
 
         public GameModel()
         {
-            payloadContentHandler = new PayloadContentHandler(this);
-        }
+        }                    
 
         private void SendState()
         {
             using (var stream = new MemoryStream())
             {
-                State.WriteContent(stream);
                 networkHandler.Send(stream.ToArray(), MessageType.Broadcast);
             }
         }
