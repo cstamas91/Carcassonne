cda8a166313c11d55c685951d0eb07bb918007f5
Csernák Tamás <altvik26@gmail.com>
2016-04-26 03:23:08 +0200

    Projekt restrukturálás

---
+++ Carcassonne/CarcassonneServer/Model/GameLogic/Game.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using CarcassonneServer.Model.Representation;
+
+namespace CarcassonneServer.Model.GameLogic
+{
+    public class Game
+    {
+        #region Declarations
+        private string guid;
+
+        public string Guid
+        {
+            get { return guid; }
+            set { guid = value; }
+        }
+
+
+        private int numberOfPlayers;
+
+        public int NumberOfPlayers
+        {
+            get { return numberOfPlayers; }
+            set { numberOfPlayers = value; }
+        }
+
+        public int CurrentPlayers
+        {
+            get { return players.Count; }
+        }
+
+        private List<Player> players;
+        private TurnHandler turnHandler;
+        private ScoreHandler scoreHandler;
+
+        public Game(int numberOfPlayers, string guid)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+            this.guid = guid;
+            
+        }
+        #endregion Declarations
+
+        #region Public methods
+        public void AddPlayer(Player player)
+        {
+            this.players.Add(player);
+        }
+
+        public void InitializeGame()
+        {
+            this.turnHandler = new TurnHandler();
+            this.scoreHandler = new ScoreHandler(players);
+        }
+        #endregion Public methods
+    }
+}
---
+++ Carcassonne/CarcassonneServer/Model/GameLogic/TurnHandler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CarcassonneServer.Model.Representation;
+
+namespace CarcassonneServer.Model.GameLogic
+{
+    /// <summary>
+    /// A játék köreit kezelő osztály. Számon tartja az eltelt körök számát, a jelenlegi játékost, és a jelenlegi kör belső állapotát.
+    /// </summary>
+    public class TurnHandler : INotifyPropertyChanged
+    {
+        #region Declaration
+        private TurnState currentTurnState;
+
+        internal TurnState CurrentTurnState
+        {
+            get { return currentTurnState; }
+            set 
+            { 
+                currentTurnState = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int turns;
+
+        public int Turns
+        {
+            get { return turns; }
+            set { turns = value; }
+        }
+
+        private Player currentPlayer;
+
+        public Player CurrentPlayer
+        {
+            get { return currentPlayer; }
+            set 
+            {
+                currentPlayer = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public TurnHandler() 
+        {
+            this.Turns = 0;
+        }
+        #endregion Declaration
+                                                                     
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion INotifyPropertyChanged
+    }
+
+    #region Exceptions
+    public class InvalidTurnStateException : Exception 
+    {
+        public InvalidTurnStateException(string message) : base(message) { }
+    }
+    #endregion Exceptions
+    /// <summary>
+    /// Egy kör belső állapotát leíró felsoroló típus.
+    /// </summary>
+    enum TurnState
+    {
+        TilePlacement,
+        MeeplePlacement,
+        Scoring
+    }
+
+    
+}
